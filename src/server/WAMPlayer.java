cfe9790ee06027c4722deb2876b6e20b3ab65732
axl8713-01 <axl8713@g.rit.edu>
2019-04-10 16:32:35 -0400

    Committing Test again, hopefully the last time.

---
+++ src/server/WAMPlayer.java
@@ -0,0 +1,54 @@
+package server;
+
+import common.WAMProtocol;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.StringJoiner;
+
+
+
+public class WAMPlayer implements WAMProtocol, Closeable {
+
+    private Socket clientSocket;
+    private Scanner networkIn;
+    private PrintStream networkOut;
+
+
+    /**
+     *
+     */
+    public WAMPlayer (Socket socket) throws IOException{
+        this.clientSocket = socket;
+        try {
+            networkIn = new Scanner(clientSocket.getInputStream()); //From Server Side
+            networkOut = new PrintStream(clientSocket.getOutputStream());//To the client
+        }catch (IOException e){}
+    }
+
+    /**
+     * Welcome is the initial connection request sent to client
+     *
+     * @param rows The rows in the game
+     * @param columns The columns in the game
+     * @param players number of players
+     * @param playerNum clients position
+     */
+    public void welcome(int rows, int columns, int players, int playerNum) {
+        StringJoiner joiner
+        networkOut.println(WELCOME + rows + columns + players + playerNum);
+    }
+
+    /**
+     * mole Up is the server informing the clients that a mole has come out of it's hole.
+     *
+     * @param moleNum The unique number of the mole that came up.
+     */
+    public void moleUp(int moleNum){networkOut.println(MOLE_UP + moleNum);}
+
+
+
+}
---
+++ src/server/WAMServer.java
@@ -0,0 +1,56 @@
+package server;
+
+import common.WAMProtocol;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class WAMServer implements WAMProtocol, Runnable {
+
+    private ServerSocket server;
+    private int rows;
+    private int col;
+    private int players;
+    private int duration;
+    private WAMPlayer[] WAMPlayers;
+
+
+    public WAMServer (int port, int rows, int col, int players, int duration) throws IOException {
+        try {server = new ServerSocket(port);
+            this.rows = rows;
+            this.col = col;
+            this.players = players;
+            this.duration = duration;
+            WAMPlayers = new WAMPlayer[players];
+        }catch (IOException e){}
+    }
+
+    public static void main(String[] args) throws IOException{
+        if (args.length != 5){
+            System.out.println("Usage: java WAMServer <game-port#> <#rows> <#columns> <#players> <game-duration-seconds>");
+        }
+
+        int port = Integer.parseInt(args[0]);
+        int rows = Integer.parseInt(args[1]);
+        int col = Integer.parseInt(args[2]);
+        int players = Integer.parseInt(args[3]);
+        int duration = Integer.parseInt(args[4]);
+        WAMServer server = new WAMServer(port, rows , col, players, duration);
+        server.run();
+
+    }
+
+    public void run(){
+        try {
+            for (int i=0 ; i<players; i++){
+                System.out.println("Waiting for player " + i);
+                Socket playerSocket = server.accept();
+                WAMPlayer player = new WAMPlayer(playerSocket);
+                WAMPlayers[i]= player;
+                player.welcome(rows,col,players,i);
+            }
+        }catch (IOException e){}
+
+    }
+}
