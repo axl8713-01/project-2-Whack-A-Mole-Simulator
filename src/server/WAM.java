7cc811a484e5f5d395b3be8eb09a1259ccd9490e
axl8713-01 <axl8713@g.rit.edu>
2019-04-10 19:46:50 -0400

    Added a Mole class within the WAM class to represent an individual mole thread.

--- src/server/WAM.java
+++ src/server/WAM.java
@@ -1,4 +1,20 @@
 package server;
 
 public class WAM {
+
+    private int rows;
+    private int cols;
+    private int duration;
+    private Mole[] moles;
+
+    public WAM(int rows, int cols, int duration){
+
+    }
+
+
+
+}
+
+public class Mole extends Thread{
+
 }
