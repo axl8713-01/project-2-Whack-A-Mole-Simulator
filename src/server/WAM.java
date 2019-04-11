ba57566424d5c8fba7bcd1b8b0a1c1db3276e83c
axl8713-01 <axl8713@g.rit.edu>
2019-04-11 18:06:35 -0400

    Working out WAMGame threading logic

--- src/server/WAM.java
+++ src/server/WAM.java
@@ -5,14 +5,14 @@ public class WAM{
     private final static Integer[] COLS = new Integer[1];
     private int rows;
     private int cols;
-    private int duration;
+//    private int duration;
 
-    public WAM(int rows, int cols, int duration){
+    public WAM(int rows, int cols/*, int duration*/){
         this.rows=rows;
         this.cols=cols;
         ROWS[1] = rows;
         COLS[1] = cols;
-        this.duration=duration;
+//        this.duration=duration;
 
 
     }
