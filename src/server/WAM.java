938254a893a81f01d9f2364670281bdbeb0db392
axl8713-01 <axl8713@g.rit.edu>
2019-04-10 20:45:02 -0400

    Worked more on game logic, stuck on creating individual mole threads in the main of WAM.

--- src/server/WAM.java
+++ src/server/WAM.java
@@ -1,20 +1,58 @@
 package server;
 
 public class WAM {
-
+    private final static Integer[] ROWS = new Integer[1];
+    private final static Integer[] COLS = new Integer[1];
     private int rows;
     private int cols;
     private int duration;
-    private Mole[] moles;
 
     public WAM(int rows, int cols, int duration){
+        this.rows=rows;
+        this.cols=cols;
+        ROWS[1] = rows;
+        COLS[1] = cols;
+        this.duration=duration;
+
 
     }
 
+    public void startHiding(Mole mole){
+
+    }
+
+    public static void main(String[] args){
+        final int rows = ROWS[0];
+        final int cols = COLS[0];
+        int total = rows * cols;
+        Thread[] moles = new Thread[total];
+        for (int i = 0 ; i < total; i++){
+            moles[i]= new Mole(i);
+        }
 
 
 }
 
 public class Mole extends Thread{
 
+        //This mole thread's unique ID
+        private int id;
+        private WAM game;
+
+
+
+        public Mole(int id){
+            this.id = id;
+
+        }
+
+        public int getID(){return id;}
+
+        @Override
+    public void run(){
+            game.startHiding(this);
+        }
+
+
+    }
 }
