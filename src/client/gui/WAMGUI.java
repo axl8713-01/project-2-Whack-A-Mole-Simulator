d7a274b08ca894c0e7954c564b43e3112225a524
axl8713-01 <axl8713@g.rit.edu>
2019-04-12 16:19:54 -0400

    Added dimensions to buttons, working on commenting and zipping up for submission.

--- src/client/gui/WAMGUI.java
+++ src/client/gui/WAMGUI.java
@@ -53,15 +53,17 @@ public class WAMGUI extends Application implements Observer<WAMBoard> {
         Scene scene=new Scene(vBox);
         stage.setTitle(" Whack A Mole!");
         stage.setScene(scene);
-        stage.setMinWidth(300);
+        stage.setMinWidth(500);
         stage.show();
     }
 
     public GridPane makeGridPane(){
         gridPane=new GridPane();
+        gridPane.setGridLinesVisible(true);
         for(int i=0; i<board.COLS; i++){
             for(int j=0; j<board.ROWS; j++){
                 Button button=new Button();
+                button.setPrefSize(150,150);
                 button.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
                 gridPane.add(button, i,j);
             }
@@ -77,29 +79,34 @@ public class WAMGUI extends Application implements Observer<WAMBoard> {
         for (int i = 0; i < board.COLS; i++) {
             for (int j = 0; j < board.ROWS; j++) {
                 Button b = new Button();
+                b.setPrefSize(150,150);
                 if (this.board.getMoleHole(i, j)) {
                     b.setBackground(new Background(new BackgroundFill(Color.BLACK, null, null)));
                     gridPane.add(b, i, j);
                 }
+                else {
+                    b.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
+                    gridPane.add(b, i, j);
+                }
             }
         }
-        WAMBoard.Result res=board.result;
-        switch(res){
-            case WON:
-                this.status.setText(" YOU WON ");
-                //endGame();
-                break;
-            case LOST:
-                this.status.setText(" YOU LOST ");
-                //endGame();
-                break;
-            case TIE:
-                this.status.setText(" TIED GAME ");
-                //endGame();
-                break;
-            default:
-                break;
-        }
+//        WAMBoard.Result res=board.result;
+//        switch(res){
+//            case WON:
+//                this.status.setText(" YOU WON ");
+//                //endGame();
+//                break;
+//            case LOST:
+//                this.status.setText(" YOU LOST ");
+//                //endGame();
+//                break;
+//            case TIE:
+//                this.status.setText(" TIED GAME ");
+//                //endGame();
+//                break;
+//            default:
+//                break;
+//        }
     }
 
     public void endGame(){
